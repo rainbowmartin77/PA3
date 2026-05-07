859094f0f19cadd578ef4da0d38e1b1536ba3131
Rainbow Martin <Shannon.Martin@trojans.dsu.edu>
2026-05-06 20:32:06 -0500

    Successfully mapped using mmap? No error.

--- psort.c
+++ psort.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdint.h>
+#include <sys/mman.h>
+
+typedef struct {
+    int32_t key;
+    char data[96]; 
+} rec;
 
 int main(int argc, char* argv[]) {
     int f = open(argv[0], O_RDONLY);
@@ -8,7 +17,20 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    struct stat data;
+    if (fstat(f, &data) == -1) {
+        perror("fstat error.");
+        close(f);
+        return 1;
+    }
 
+    rec *mapped = mmap(NULL, data.st_size, PROT_READ, MAP_PRIVATE, f, 0);
+    if (mapped == MAP_FAILED) {
+        perror("mmap error");
+        return 1;
+    }
+
+    close(f);
 
     return 0;
 }
